4d8d3bb85ae64dd77bae98a6dd0527289af1a810
Leaderdeltabrainace <145682543+Leaderdeltabrainace@users.noreply.github.com>
2024-01-05 14:04:18 +0530

    immit project

---
+++ app/src/main/java/com/example/carify2/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.carify2
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show()
+
+        val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
+
+        bottomBar.setOnItemSelectedListener { menuItem ->
+
+
+            if (menuItem.itemId == R.id.nav_Guard) {
+
+                inflateFragment(GuardFragment.newInstance())
+            } else if (menuItem.itemId == R.id.nav_home) {
+
+                inflateFragment(HomeFragment.newInstance())
+
+            } else if (menuItem.itemId == R.id.nav_dashboard) {
+
+                inflateFragment(DashboardFragment.newInstance())
+
+            } else if (menuItem.itemId == R.id.nav_Profile) {
+
+                inflateFragment(ProfileFragment.newInstance())
+
+            }
+
+
+            true
+        }
+
+
+    }
+
+    private fun inflateFragment(newInstance: Fragment) {
+
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, newInstance)
+        transaction.commit()
+
+
+    }
+
+
+}
+
+
